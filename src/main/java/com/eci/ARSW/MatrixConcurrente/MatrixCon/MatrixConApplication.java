b3e1a5b2bc1122cdd2b4c1c62cc6e04f6837162a
David Barbosa <david.barbosa-g@mail.escuelaing.edu.co>
2025-06-25 18:52:12 -0500

    Primer commit del proyecto Matrix concurrente

---
+++ src/main/java/com/eci/ARSW/MatrixConcurrente/MatrixCon/MatrixConApplication.java
@@ -0,0 +1,13 @@
+package com.eci.ARSW.MatrixConcurrente.MatrixCon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatrixConApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatrixConApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eci/ARSW/MatrixConcurrente/MatrixCon/MatrixConApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eci.ARSW.MatrixConcurrente.MatrixCon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatrixConApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
